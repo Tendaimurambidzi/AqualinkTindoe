d92cb9ece073b4e4abea214f67c5fbda9220b45a
Tendai Murambidzi <mukuemrambidzi@gmail.com>
2026-01-15 15:32:42 +0300

    Simplify shareDriftLink to share website URL directly

--- src/services/driftService.ts
+++ src/services/driftService.ts
@@ -1,16 +1,8 @@
-import functions from '@react-native-firebase/functions';
 import { Share } from 'react-native';
 
 export const shareDriftLink = async (docId: string, channel: string, title: string) => {
   try {
-    // Call Firebase Function to generate link
-    const result = await functions().httpsCallable('generateShareLink')({
-      docId,
-      channel,
-      title,
-    });
-
-    const link = result.data.link;
+    const link = `https://tendaimurambidzi.github.io/AqualinkTindoe/drift?docId=${docId}&channel=${channel}`;
 
     // Share the link
     await Share.share({
